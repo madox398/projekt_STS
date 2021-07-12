fb370e3853826075c015e07380a5716bb207e7c1
pzamor <pzamor@softsystem.pl>
2021-07-12 12:25:16 +0200

    Repair name sending from name input

--- src/main/resources/static/scitpt.js
+++ src/main/resources/static/scitpt.js
@@ -8,6 +8,9 @@ $(window).on('load', function(){
     var keyDown=[];
     var keyDownTextLength=0;
 
+    var charsToSend=[];
+    var nameIsDone=false;
+
     const maxLengthOfText = 150;
 
     const $textArea = $("#text");
@@ -27,12 +30,52 @@ $(window).on('load', function(){
         var str = $name.val();
         if (str.length > 1) {
             $textArea.removeAttr('disabled');
+            nameIsDone=true;
         } else {
             $textArea.attr('disabled', 'disabled');
+            nameIsDone=false;
         }
     }));
+    $name.keydown(function (event) {
+        //określa liczbę znaków w polu tekstowym przed wpisaniem znaku
+        keyDownTextLength=$textArea.val().length;
+        //Czas od poprzedniego wciśnięcia klawisza zacznij liczyć
+        //gdy zostanie wciśnięty pierwszy klawisz
+        if (firstKey) {
+            firstKey = !firstKey;
+            timeToPreviousTemp = Date.now();
+        }
 
-    $name.add($textArea).keydown(function (event) {
+        if (!keyDown[event.which]) {
+            //Eliminuje problem długiego wciśnięcia przycisku
+            keyDown[event.which] = true;
+            //Zmienna tymczasowa do mierzenia czasu wciśnięcia
+            timeTemp[event.which] = new Date();
+            //Czas od poprzedniego klawisza
+            timeToPrevious[event.which] = Date.now() - timeToPreviousTemp;
+        }
+        timeToPreviousTemp = Date.now();
+    })
+        .keyup(function (event) {
+            if (keyDownTextLength < maxLengthOfText) {
+                //Jeśli przycisk został puszczony przypisz false
+                keyDown[event.which] = false;
+                //Długość wciśnięcia
+                timePress[event.which] = Date.now() - timeTemp[event.which];
+                personName = $name.val();
+                saveNameToSendLater(event);
+            } else {
+                $spanKeyId.text("Przekroczyłeś maksymalną liczbę znaków");
+                $paragraphKeyId.css("display", "none");
+            }
+        })
+    $textArea.on("focus", function () {
+        if(nameIsDone){
+            nameIsDone = !nameIsDone;
+            sendOnlyNameToDatabase();
+        }
+    })
+    $textArea.keydown(function (event) {
         //określa liczbę znaków w polu tekstowym przed wpisaniem znaku
         keyDownTextLength=$textArea.val().length;
         //Czas od poprzedniego wciśnięcia klawisza zacznij liczyć
@@ -53,7 +96,7 @@ $(window).on('load', function(){
         timeToPreviousTemp = Date.now();
     })
 
-    $name.add($textArea).keyup(function (event) {
+    $textArea.keyup(function (event) {
         if (keyDownTextLength < maxLengthOfText) {
             //Jeśli przycisk został puszczony przypisz false
             keyDown[event.which] = false;
@@ -108,5 +151,35 @@ $(window).on('load', function(){
         timePress[idKey]=0;
         personName="";
         return false;
+    }
+    function sendOnlyNameToDatabase(){
+        var i =0;
+        for (i; i < charsToSend.length; i++) {
+            var old = JSON.stringify(charsToSend[i]).replace('##', personName)
+            charsToSend[i] = JSON.parse(old);
+            console.info(charsToSend[i]);
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json; charset:utf-8",
+                    url: "http://localhost:8080/keys/add",
+                    data: charsToSend[i],
+                    success: function(){
+                        setTimeout(function() {
+                        }, 2);
+                    }
+                })
+        }
+        return false;
+    }
+
+    function saveNameToSendLater(key){
+        var lastIndex = charsToSend.length;
+        console.log(lastIndex);
+        charsToSend[lastIndex] = JSON.stringify({
+            "name":"##",
+            "keyCode": key.code,
+            "timePressed": timePress[key.which],
+            "timeToNextChar": timeToPrevious[key.which]
+        })
     }
 })
