57e77605a102ff171914f194d30c180004297e0c
pzamor <pzamor@softsystem.pl>
2021-07-12 12:33:20 +0200

    Add export from database to csv

--- src/main/java/com/example/demo/controller/BackEndController.java
+++ src/main/java/com/example/demo/controller/BackEndController.java
@@ -11,8 +11,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -89,5 +96,29 @@ public class BackEndController {
         response.addHeader("Content-Type","application/json");
         Pageable pageable = PageRequest.of(page,limit);
         return repo.findKeyDataByName(name,pageable);
+    }
+    //Export to CSV
+    @GetMapping("/keys/export")
+    public void exportToCSV(HttpServletResponse response) throws IOException {
+        response.setContentType("text/csv");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".csv";
+        response.setHeader(headerKey, headerValue);
+
+        Iterable<KeyData> listUsers = repo.findAll();
+
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String[] csvHeader = {"ID", "name", "key_code", "time_pressed", "time_to_next_char"};
+        String[] nameMapping = {"id", "name", "keyCode", "timePressed", "timeToNextChar"};
+
+        csvWriter.writeHeader(csvHeader);
+
+        for (KeyData keyData : listUsers) {
+            csvWriter.write(keyData, nameMapping);
+        }
+        csvWriter.close();
     }
 }
